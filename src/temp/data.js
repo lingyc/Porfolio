dced3632b6785ddf00bc03ee955a47786d98d9b2
Ling Yu Chen <lingchen215@gmail.com>
2016-11-08 23:32:59 -0800

    Use actual data

--- src/temp/data.js
+++ src/temp/data.js
@@ -3,102 +3,108 @@ const projectInfo = [
 		id: '0000',
 		name: 'neARby',
 		description: 'Mobile Application',
-		thumbnail: 'http://imagecache5d.allposters.com/watermarker/76-7640-117G300Z.jpg?ch=949&cw=633',
+		thumbnail: 'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/000.png',
 		thumbnails: [
-			'http://imagecache5d.allposters.com/watermarker/76-7640-117G300Z.jpg?ch=949&cw=633',
-			'http://imagecache5d.allposters.com/watermarker/56-5698-2SSUG00Z.jpg',
-			'http://imagecache5d.allposters.com/watermarker/17-1723-KK53D00Z.jpg'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/000.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/001.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/003.png'
 		],
 		img: [
-			'http://imagecache5d.allposters.com/watermarker/76-7640-117G300Z.jpg?ch=949&cw=633',
-			'http://imagecache5d.allposters.com/watermarker/56-5698-2SSUG00Z.jpg',
-			'http://imagecache5d.allposters.com/watermarker/17-1723-KK53D00Z.jpg'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/000.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/001.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0002/003.png'
 		],
+		link: 'https://calculatingpineapples.github.io/neARby_web/',
 		detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\nCurabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. '
 	},
 	{
 		id: '0001',
 		name: 'ReelPals',
 		description: 'Web Application',
-		thumbnail: 'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
+		thumbnail: 'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/000.png',
 		thumbnails: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/000.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/001.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/002.png'
 		],
 		img: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/000.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/001.png',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0001/002.png'
 		],
+		link: 'https://reelpals.herokuapp.com/',
 		detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\nCurabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. '
 	},
 	{
 		id: '0002',
 		name: 'FoodBuddy',
 		description: 'Web Application',
-		thumbnail: 'http://imagecache5d.allposters.com/watermarker/56-5698-2SSUG00Z.jpg',
+		thumbnail: 'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0003/000',
 		thumbnails: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0003/000',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0003/001'
 		],
 		img: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0003/000',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0003/001'
 		],
+		link: 'http://ec2-54-68-219-27.us-west-2.compute.amazonaws.com:8000/',
 		detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\nCurabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. '
 	},
 	{
 		id: '0003',
 		name: 'WishSpots',
 		description: 'Web Application',
-		thumbnail: 'http://imagecache5d.allposters.com/watermarker/17-1723-KK53D00Z.jpg',
+		thumbnail: 'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0004/000.png',
 		thumbnails: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0004/000.png'
 		],
 		img: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0004/000.png'
 		],
+		link: 'https://wishspots.herokuapp.com/',
 		detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\nCurabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. '
 	},
 	{
 		id: '0004',
 		name: 'Huatai Design Competition',
 		description: 'Architectural Design',
-		thumbnail: 'http://imagecache5d.allposters.com/watermarker/56-5662-2HFUG00Z.jpg',
+		thumbnail: 'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/bnk-c19a.jpg',
 		thumbnails: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/C15.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/Untitled-3.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/bnk-c19a.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/c10cc+a0001.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/c14.jpg'
 		],
 		img: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/C15.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/Untitled-3.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/bnk-c19a.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/c10cc+a0001.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0005/c14.jpg'
 		],
+		link: 'https://issuu.com/lingchen215/docs/oct_2015ss',
 		detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\nCurabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. '
 	},
 	{
 		id: '0005',
 		name: 'South Water Residence',
 		description: 'Architectural Design',
-		thumbnail: 'http://imagecache5d.allposters.com/watermarker/15-1555-PN9DD00Z.jpg',
+		thumbnail: 'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/landscape-10_9.jpg',
 		thumbnails: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/232.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/landscape-10_9.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/nk-c06-BT.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/nk-c07-BT.jpg'
 		],
 		img: [
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655',
-			'http://imagecache5d.allposters.com/watermarker/21-2126-GJJED00Z.jpg?ch=916&cw=655'
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/232.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/landscape-10_9.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/nk-c06-BT.jpg',
+			'https://s3-us-west-2.amazonaws.com/app-porfolio/projects/0006/nk-c07-BT.jpg'
 		],
+		link: 'https://issuu.com/lingchen215/docs/oct_2015ss',
 		detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\nCurabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. '
 	}
 ];
